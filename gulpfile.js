75ec8829df1e4172c6320372985a5f04fa075e44
markbulavchyk <marksaitiev@gmail.com>
2025-05-12 00:00:01 -0600

    mod

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
 
-gulp.task('hello', function (done) {
-    console.log('hello, from Gulp');
-    done();
+
+const fileIncludeSetting = {
+    prefix: '@@', // благодаря этому сможем импортировать файлы
+    basepath: '@file'
+};
+
+gulp.task('includeFiles', function () {
+    return gulp
+        .src('./src/*.html')
+        .pipe(fileInclude(fileIncludeSetting))
+        .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', gulp.series('hello'));
